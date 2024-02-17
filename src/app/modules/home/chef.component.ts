aac0db2c83695e9c478b5f9dc89e4ed8e9ae100c
Shawn Dai <sdai17@asu.edu>
2024-02-17 00:49:37 -0700

    drawar

--- src/app/modules/home/chef.component.ts
+++ src/app/modules/home/chef.component.ts
@@ -6,16 +6,24 @@ import Pizza from 'app/interface/pizza';
 import { PizzzService } from 'app/pizzz.service';
 import {MatCardModule} from '@angular/material/card';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
     selector     : 'chef-home',
     templateUrl  : './chef.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone   : true,
-    imports      : [MatButtonModule, RouterLink, MatIconModule, MatCardModule, NgFor],
+    imports      : [MatButtonModule, RouterLink, MatIconModule, MatCardModule, NgFor, FormsModule, MatCheckboxModule, MatSidenavModule],
 })
 export class ChefMainComponent implements OnInit
-{
+{   
+
+    events: string[] = [];
+    opened: boolean;
+
+    shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
 
     pizzas: Pizza[];
     /**
