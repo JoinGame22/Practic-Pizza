cba171c7bf5b0392ff84302acaa06a25755e1ffb
Shawn Dai <sdai17@asu.edu>
2024-02-17 18:18:08 -0700

    ownerpage

--- src/app/modules/test/test.component.ts
+++ src/app/modules/test/test.component.ts
@@ -1,79 +1,103 @@
-import { CommonModule } from '@angular/common';
-import {AfterViewInit, Component, NgModule, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation} from '@angular/core';
-import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import {FMComponent} from "../component/fm65c0787cb1a0bc7c6fa3d459.component";
-import { MatFormFieldModule } from '@angular/material/form-field';
+import Pizza from 'app/interface/pizza';
+import Toppings from 'app/interface/topping';
+import { PizzaService } from 'app/pizza.service';
+import {MatCardModule} from '@angular/material/card';
+import { NgFor, JsonPipe} from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
+import { ToppingService } from 'app/topping.service';
+import { create } from 'lodash';
 
 @Component({
-    selector     : 'test',
-    standalone   : true,
+    selector     : 'test-home',
     templateUrl  : './test.component.html',
-    encapsulation: ViewEncapsulation.None,    
-    imports:[
-        FMComponent,MatButtonModule, RouterLink, MatIconModule, CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule
-    ]
+    encapsulation: ViewEncapsulation.None,
+    standalone   : true,
+    imports      : [MatButtonModule, RouterLink, MatIconModule, MatCardModule, NgFor, FormsModule, MatCheckboxModule, MatSidenavModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, JsonPipe],
 })
-export class TestComponent implements  OnInit 
-{
+export class TestComponent implements OnInit
+{   
+    PizzaName = "";
+    PizzaPrice = 0;
+    PizzaTopping = "";
+    PizzaDescription = "";
+    
+    createPizza: Pizza = {
+        
+        name: 'PizzaName',
+        pic: '',
+        price: this.PizzaPrice,
+        topping: 'PizzaTopping',
+        description: 'PizzaDescription'
+    };
+
+    toppings: FormGroup;
+
+    pizzas: Pizza[];
+    Topping:Toppings[];
+
     /**
      * Constructor
      */
-    @ViewChild('test', {read:ViewContainerRef}) testContainer!: ViewContainerRef;
-  
-    reactiveComponent={
-        component:{
-          content:{
-              question: "input the question here?",
-              options:{
-                option1: {
-                    key:"1",
-                    value:"option 1"
-                },
-                option2:{
-                    key:"2",
-                    value:"option 2"
-                },
-                option3:{
-                    key:"3",
-                    value:"option 3"
-                }
-              }
 
-          },
-          ui:{},      
-        },
-        runtime:{
-          data:{
-             
-          },
-          score:{},
-          feedback:{}
-        }
-      };
-  
-    constructor()
-    {
+    constructor(private _pizzaService: PizzaService, private _formBuilder: FormBuilder, private _toppingService: ToppingService)
+    {}
 
-
-    }
-
-    StartTest(){
-
-        console.log('container:' + this.testContainer);
-        const fmComponentRef  = this.testContainer!.createComponent(FMComponent);
-        (fmComponentRef as any).instance.data = this.reactiveComponent;
-        fmComponentRef.changeDetectorRef.markForCheck();
-
-    }
 
     ngOnInit(): void {
+        
+        this._pizzaService.findAllPizzas().subscribe(
+            response=>{
+                this.pizzas = response;
+                console.log(this.pizzas);
+            }
+        )
+
+       
 
         
+        this._toppingService.findAllToppings().subscribe(
+            response=>{
+                this.Topping = response;
+                console.log(this.Topping);
+            }
+        )   
+
+        this.toppings = this._formBuilder.group({});
+        
+        this.Topping.forEach(element => {
+          this.toppings.addControl(element.name, new FormControl(false)); // Initialize with false
+        });
+
+    }
+
+
+    CreatePizza(){
+
+        this._pizzaService.createPizza(this.createPizza).subscribe(
+            response=>{
+
+                console.log('Pizza created successfully:', response);
+              },
+              error => {
+                console.error('Error creating pizza:', error);
+              }
+            );
+
+    }
+
+    DeletePizza(){
+        
+    }
+
+    EditPizza(){
         
     }
 }
